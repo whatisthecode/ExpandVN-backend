94bccbec6583707ed013396bdd90260529940105
whatisthecode <hungtienphan1995@gmail.com>
2023-08-11 14:55:16 +0700

    Fix syntax

--- index.js
+++ index.js
@@ -6,7 +6,64 @@ const endpoint = "https://graph.zalo.me/v2.0/me/info";
 const secretKey = process.env.ZALO_APP_SECRET_KEY || "";
 
 const app = express()
-app.all('/user-phone', (req, res) => {
+app.get('/user-phone', (req, res) => {
+    const userAccessToken = req.headers["X-User-Access-Token"] || req.headers["x-user-access-token"];
+    const token = req.headers["X-Token"] || req.headers["x-token"];
+
+    if(!secretKey) return res.status(200).json({
+        code: 500,
+        message: "Permission denined"
+    });
+
+    if(!userAccessToken) return res.status(200).json({
+        code: 400,
+        message: "Missing user access token"
+    });
+
+    if(!token) return res.status(200).json({
+        code: 401,
+        message: "Missing token"
+    });
+
+    const options = {
+        url: endpoint,
+        headers: {
+            access_token: userAccessToken,
+            code: token,
+            secret_key: secretKey
+        }
+    };
+
+    request(options, (error, response, body) => {
+        if (error) {
+            // console.error("Error:", error);
+            return res.status(200).json({
+                code: error.code,
+                message: error.message,
+                input: options
+            })
+        } else {
+            // console.log("Response Code:", response.statusCode);
+            // console.log("Response Body:", body);
+            try {
+                const data = JSON.parse(body);
+                return res.status(200).json({
+                    code: !data.error ? response.statusCode : data.error,
+                    data: !data.error ? data.data : undefined,
+                    message: data.error ? data.message : undefined
+                });
+            }
+            catch(e){
+                const data = JSON.parse(body);
+                return res.status(500).json({
+                    code: 500,
+                    messge: e.messge
+                });
+            }
+        }
+    });
+})
+app.get('/user-location', (req, res) => {
     const userAccessToken = req.headers["X-User-Access-Token"] || req.headers["x-user-access-token"];
     const token = req.headers["X-Token"] || req.headers["x-token"];
 
