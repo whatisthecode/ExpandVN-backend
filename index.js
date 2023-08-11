9c0c840103c3ee87cb0d8a9ce19c1071c3fdccbd
Hưng Phan <hungphan@Hungs-Mac-mini.local>
2023-08-11 13:55:11 +0700

    Update zalo api

--- index.js
+++ index.js
@@ -1,7 +1,54 @@
 const express = require('express')
+const request = require("request");
+
+const endpoint = "https://graph.zalo.me/v2.0/me/info";
+
+const secretKey = process.env.ZALO_APP_SECRET_KEY || "";
+
 const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+app.all('/user-phone', (req, res) => {
+    const userAccessToken = req.headers["X-User-Access-Token"] || req.headers["x-user-access-token"];
+    const token = req.headers["X-Token"] || req.headers["x-token"];
+
+    if(!secretKey) return res.status(200).json({
+        code: 500,
+        message: "Permission denined"
+    });
+
+    if(!userAccessToken) return res.status(200).json({
+        code: 400,
+        message: "Missing user access token"
+    });
+
+    if(!token) return res.tatus(200).json({
+        code: 401,
+        message: "Missing token"
+    });
+
+    const options = {
+        url: endpoint,
+        headers: {
+            access_token: userAccessToken,
+            code: token,
+            secret_key: secretKey
+        }
+    };
+
+    request(options, (error, response, body) => {
+        if (error) {
+            // console.error("Error:", error);
+            return res.status(200).json({
+                code: error.code,
+                message: error.message
+            })
+        } else {
+            // console.log("Response Code:", response.statusCode);
+            // console.log("Response Body:", body);
+            return res.status(200).json({
+                code: response.statusCode,
+                data: body
+            });
+        }
+    });
 })
 app.listen(process.env.PORT || 3000)
