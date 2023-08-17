ca2ca2e07ec09e4b45d62d875dd0c2956e1407d1
whatisthecode <hungtienphan1995@gmail.com>
2023-08-17 16:18:15 +0700

    Verify zalo app

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require('express')
 const request = require("request");
+const bodyParser = require("body-parser");
 const cors = require('cors')
+const { createHash } = require("crypto");
+const base64url = require("base64url")
 
 const endpoint = "https://graph.zalo.me/v2.0/me/info";
 
-const secretKey = process.env.ZALO_APP_SECRET_KEY || "";
+const secretKey = process.env.ZALO_APP_SECRET_KEY || ""
 
-const app = express()
+const app = express();
 
 app.use(cors());
+app.use(bodyParser.json());
 
 app.get('/user-phone', (req, res) => {
     const userAccessToken = req.headers["X-User-Access-Token"] || req.headers["x-user-access-token"];
@@ -140,7 +144,7 @@ app.get('/user-location', (req, res) => {
                                 else if(r.types[0] === "premise" && !location.premise) {
                                     location.premise = r.formatted_address;
                                 }
-                            }) || undefined;
+                            });
 
                             return res.status(200).json({
                                 code: !dataa.error ? responsee.statusCode : dataa.error,
@@ -171,5 +175,40 @@ app.get('/user-location', (req, res) => {
             }
         }
     });
+})
+
+app.get("/generate-challenge-code", (req, res) => {
+    const length = 43;
+    let verifierCode = "";
+    const sets = [];
+    for(let i = 0; i < length; i++) {
+        const set = Math.floor(Math.random() * 3) + 1;
+        sets.push(set);
+        if(set === 1) {
+            const code = Math.floor(Math.random() * 25) + 65;
+            verifierCode += String.fromCharCode(code);
+        }
+        else if(set === 2){
+            const code = Math.floor(Math.random() * 25) + 97;
+            verifierCode += String.fromCharCode(code);
+        }
+        else {
+            const code = Math.floor(Math.random() * 10) + 48;
+            verifierCode += String.fromCharCode(code);
+        }
+    }
+
+    const challengeCode = base64url(Buffer.from(createHash('sha256').update(verifierCode).digest("hex")).toString());
+
+    res.status(200).json({
+        verifierCode,
+        challengeCode
+    });
+});
+
+app.get("/verify-app", (req, res) => {
+    const query = req.query;
+
+    res.status(200).json(query);
 })
 app.listen(process.env.PORT || 3000)
